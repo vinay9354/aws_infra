c017a8dba1061a625400c62f935779973f515e39
vinay9354 <amaravinaykumar@gmail.com>
2025-11-27 22:17:01 +0530

    updated backend config

---
+++ dev/backend.tf
@@ -0,0 +1,31 @@
+# Terraform Backend Configuration for Dev Environment
+terraform {
+  backend "s3" {
+    bucket       = "vinay-terraform-state-dev"
+    key          = "aws_infra/dev/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+
+# ----------------------------------
+# Backend Bucket for Terraform State
+# ----------------------------------
+
+module "backen-bucket" {
+  source = "../modules/data/s3"
+  bucket_name       = "vinay-terraform-state-dev"
+    force_destroy     = false
+    enable_versioning = true
+  
+    # Public access settings (defaults shown for clarity)
+    acl                     = "private"
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+    object_ownership        = "BucketOwnerPreferred"
+
+}
--- dev/provider.tf
+++ dev/provider.tf
@@ -2,19 +2,19 @@ terraform {
   required_version = "~> 1.12.1"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.22.1"
     }
   }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region              = var.aws_region
   allowed_account_ids = var.allowed_account_ids
   default_tags {
     tags = {
-      Owner       = "vinay"
-      Managed_by  = "terraform"
+      Owner      = "vinay"
+      Managed_by = "terraform"
     }
   }
 }
