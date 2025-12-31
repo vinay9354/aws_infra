8e58cb58bfe24bfe3306d7724483b995d9594f73
vinay9354 <amaravinaykumar@gmail.com>
2025-12-31 10:37:55 +0530

    Comment out EKS cluster module and outputs

--- dev/locals.tf
+++ dev/locals.tf
@@ -219,217 +219,217 @@ locals {
   }
 }
 
-# -------------------------
-# EKS Cluster Locals
-# -------------------------
-locals {
-  eks_cluster_config = {
-    # --------------------------------------------------------------------------
-    # General Cluster Configuration
-    # --------------------------------------------------------------------------
-    cluster_name    = "vinay-dev-infra-eks-cluster"
-    cluster_version = "1.34" # Kubernetes version
+# # -------------------------
+# # EKS Cluster Locals
+# # -------------------------
+# locals {
+#   eks_cluster_config = {
+#     # --------------------------------------------------------------------------
+#     # General Cluster Configuration
+#     # --------------------------------------------------------------------------
+#     cluster_name    = "vinay-dev-infra-eks-cluster"
+#     cluster_version = "1.34" # Kubernetes version
 
-    # --------------------------------------------------------------------------
-    # Networking Configuration
-    # --------------------------------------------------------------------------
-    # List of subnet IDs where the EKS cluster control plane (ENIs) and nodes will be placed.
-    # For private clusters, these should be private subnets.
-    subnet_ids = [
-      module.private_subnets["eks-1"].subnet_id,
-      module.private_subnets["eks-2"].subnet_id,
-      module.private_subnets["eks-3"].subnet_id
-    ]
+#     # --------------------------------------------------------------------------
+#     # Networking Configuration
+#     # --------------------------------------------------------------------------
+#     # List of subnet IDs where the EKS cluster control plane (ENIs) and nodes will be placed.
+#     # For private clusters, these should be private subnets.
+#     subnet_ids = [
+#       module.private_subnets["eks-1"].subnet_id,
+#       module.private_subnets["eks-2"].subnet_id,
+#       module.private_subnets["eks-3"].subnet_id
+#     ]
 
-    # Cluster Endpoint Access Control
-    # It is recommended to keep public access disabled for security.
-    cluster_endpoint_private_access      = true
-    cluster_endpoint_public_access       = false
-    cluster_endpoint_public_access_cidrs = []
+#     # Cluster Endpoint Access Control
+#     # It is recommended to keep public access disabled for security.
+#     cluster_endpoint_private_access      = true
+#     cluster_endpoint_public_access       = false
+#     cluster_endpoint_public_access_cidrs = []
 
-    # --------------------------------------------------------------------------
-    # Access Management (Authentication & Authorization)
-    # --------------------------------------------------------------------------
-    # Access entries for granting permissions to IAM principals (Users/Roles)
-    access_entries = {
-      admin_user = {
-        principal_arn = "arn:aws:iam::516311263797:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_5fe30505cf49ba4a"
-        type          = "STANDARD"
+#     # --------------------------------------------------------------------------
+#     # Access Management (Authentication & Authorization)
+#     # --------------------------------------------------------------------------
+#     # Access entries for granting permissions to IAM principals (Users/Roles)
+#     access_entries = {
+#       admin_user = {
+#         principal_arn = "arn:aws:iam::516311263797:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_5fe30505cf49ba4a"
+#         type          = "STANDARD"
 
-        policy_associations = {
-          admin = {
-            policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-            access_scope = {
-              type = "cluster"
-            }
-          }
-        }
-      }
-    }
+#         policy_associations = {
+#           admin = {
+#             policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#             access_scope = {
+#               type = "cluster"
+#             }
+#           }
+#         }
+#       }
+#     }
 
-    # Enable IAM Roles for Service Accounts (IRSA) for fine-grained pod permissions
-    enable_irsa = true
+#     # Enable IAM Roles for Service Accounts (IRSA) for fine-grained pod permissions
+#     enable_irsa = true
 
-    # --------------------------------------------------------------------------
-    # Logging & Encryption
-    # --------------------------------------------------------------------------
-    enable_cluster_logging = true
-    create_kms_key         = false # Set to true to enable envelope encryption for Kubernetes secrets
+#     # --------------------------------------------------------------------------
+#     # Logging & Encryption
+#     # --------------------------------------------------------------------------
+#     enable_cluster_logging = true
+#     create_kms_key         = false # Set to true to enable envelope encryption for Kubernetes secrets
 
-    # --------------------------------------------------------------------------
-    # Scaling & Reliability
-    # --------------------------------------------------------------------------
-    # Control Plane Scaling (Standard or High)
-    control_plane_scaling_config = {
-      tier = "standard"
-    }
+#     # --------------------------------------------------------------------------
+#     # Scaling & Reliability
+#     # --------------------------------------------------------------------------
+#     # Control Plane Scaling (Standard or High)
+#     control_plane_scaling_config = {
+#       tier = "standard"
+#     }
 
-    # Zonal Shift (Automatic failover away from unhealthy AZs)
-    zonal_shift_config = {
-      enabled = true
-    }
+#     # Zonal Shift (Automatic failover away from unhealthy AZs)
+#     zonal_shift_config = {
+#       enabled = true
+#     }
 
-    # --------------------------------------------------------------------------
-    # Maintenance & Upgrades
-    # --------------------------------------------------------------------------
-    upgrade_policy = {
-      support_type = "STANDARD" # Options: STANDARD, EXTENDED
-    }
+#     # --------------------------------------------------------------------------
+#     # Maintenance & Upgrades
+#     # --------------------------------------------------------------------------
+#     upgrade_policy = {
+#       support_type = "STANDARD" # Options: STANDARD, EXTENDED
+#     }
 
-    deletion_protection = false
+#     deletion_protection = false
 
-    # --------------------------------------------------------------------------
-    # Remote Network Configuration (EKS Hybrid Nodes support)
-    # --------------------------------------------------------------------------
-    # Uncomment and configure if using EKS Hybrid Nodes
-    # remote_network_config = {
-    #   remote_node_networks = {
-    #     cidrs = ["10.0.0.0/8"]  # On-premises network CIDR
-    #   }
-    #   remote_pod_networks = {
-    #     cidrs = ["172.30.0.0/16"]  # Pod network CIDR for hybrid nodes
-    #   }
-    # }
+#     # --------------------------------------------------------------------------
+#     # Remote Network Configuration (EKS Hybrid Nodes support)
+#     # --------------------------------------------------------------------------
+#     # Uncomment and configure if using EKS Hybrid Nodes
+#     # remote_network_config = {
+#     #   remote_node_networks = {
+#     #     cidrs = ["10.0.0.0/8"]  # On-premises network CIDR
+#     #   }
+#     #   remote_pod_networks = {
+#     #     cidrs = ["172.30.0.0/16"]  # Pod network CIDR for hybrid nodes
+#     #   }
+#     # }
 
-    # --------------------------------------------------------------------------
-    # Node Groups Configuration
-    # --------------------------------------------------------------------------
-    managed_node_groups = {
-      # Spot Instance Node Group (Cost-Optimized)
-      spot_nodes = {
-        name            = "dev-spot-nodes"
-        use_name_prefix = true
+#     # --------------------------------------------------------------------------
+#     # Node Groups Configuration
+#     # --------------------------------------------------------------------------
+#     managed_node_groups = {
+#       # Spot Instance Node Group (Cost-Optimized)
+#       spot_nodes = {
+#         name            = "dev-spot-nodes"
+#         use_name_prefix = true
 
-        # Scaling Settings
-        min_size     = 1
-        max_size     = 2
-        desired_size = 1
+#         # Scaling Settings
+#         min_size     = 1
+#         max_size     = 2
+#         desired_size = 1
 
-        # Instance Configuration
-        capacity_type  = "SPOT"
-        instance_types = ["t3a.medium"]
-        ami_type       = "AL2023_x86_64_STANDARD"
-        disk_size      = 25
+#         # Instance Configuration
+#         capacity_type  = "SPOT"
+#         instance_types = ["t3a.medium"]
+#         ami_type       = "AL2023_x86_64_STANDARD"
+#         disk_size      = 25
 
-        # Availability Settings
-        update_config = {
-          max_unavailable_percentage = 25
-        }
+#         # Availability Settings
+#         update_config = {
+#           max_unavailable_percentage = 25
+#         }
 
-        # Kubernetes Labels
-        labels = {
-          "node-type"     = "spot"
-          "workload"      = "general"
-          "capacity-type" = "spot"
-          "environment"   = "dev"
-          "managed"       = "true"
-        }
+#         # Kubernetes Labels
+#         labels = {
+#           "node-type"     = "spot"
+#           "workload"      = "general"
+#           "capacity-type" = "spot"
+#           "environment"   = "dev"
+#           "managed"       = "true"
+#         }
 
-        # Tags
-        tags = {
-          NodeGroup      = "spot-optimized"
-          CostAllocation = "dev-spot"
-        }
+#         # Tags
+#         tags = {
+#           NodeGroup      = "spot-optimized"
+#           CostAllocation = "dev-spot"
+#         }
 
-        # Storage Configuration
-        create_launch_template = true
-        block_device_mappings = {
-          root = {
-            device_name = "/dev/xvda"
-            ebs = {
-              volume_size           = 8
-              volume_type           = "gp3"
-              delete_on_termination = true
-              encrypted             = true
-              iops                  = 3000
-              throughput            = 125
-            }
-          }
-        }
+#         # Storage Configuration
+#         create_launch_template = true
+#         block_device_mappings = {
+#           root = {
+#             device_name = "/dev/xvda"
+#             ebs = {
+#               volume_size           = 8
+#               volume_type           = "gp3"
+#               delete_on_termination = true
+#               encrypted             = true
+#               iops                  = 3000
+#               throughput            = 125
+#             }
+#           }
+#         }
 
-        # IAM Configuration
-        create_iam_role = true
-        iam_role_additional_policies = {
-          ssm_access = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-        }
-      }
+#         # IAM Configuration
+#         create_iam_role = true
+#         iam_role_additional_policies = {
+#           ssm_access = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+#         }
+#       }
 
-      # NOTE: Example On-Demand Node Group (Uncomment to use)
-      # dev_ondemand_nodes = {
-      #   name            = "dev-ondemand-nodes"
-      #   use_name_prefix = true
-      #   min_size        = 1
-      #   max_size        = 3
-      #   desired_size    = 1
-      #   capacity_type   = "ON_DEMAND"
-      #   instance_types  = ["t3.medium"]
-      #   ami_type        = "AL2023_x86_64_STANDARD"
-      #   disk_size       = 30
-      #   update_config   = { max_unavailable_percentage = 25 }
-      #   labels          = { "node-type" = "ondemand", "workload" = "critical" }
-      #   create_launch_template = true
-      #   create_iam_role = true
-      # }
-    }
+#       # NOTE: Example On-Demand Node Group (Uncomment to use)
+#       # dev_ondemand_nodes = {
+#       #   name            = "dev-ondemand-nodes"
+#       #   use_name_prefix = true
+#       #   min_size        = 1
+#       #   max_size        = 3
+#       #   desired_size    = 1
+#       #   capacity_type   = "ON_DEMAND"
+#       #   instance_types  = ["t3.medium"]
+#       #   ami_type        = "AL2023_x86_64_STANDARD"
+#       #   disk_size       = 30
+#       #   update_config   = { max_unavailable_percentage = 25 }
+#       #   labels          = { "node-type" = "ondemand", "workload" = "critical" }
+#       #   create_launch_template = true
+#       #   create_iam_role = true
+#       # }
+#     }
 
-    # --------------------------------------------------------------------------
-    # Cluster Add-ons
-    # --------------------------------------------------------------------------
-    cluster_addons = {
-      vpc-cni = {
-        addon_version               = "v1.20.4-eksbuild.1"
-        resolve_conflicts_on_create = "OVERWRITE"
-        resolve_conflicts_on_update = "OVERWRITE"
-      }
-      kube-proxy = {
-        addon_version               = "v1.34.0-eksbuild.4"
-        resolve_conflicts_on_create = "OVERWRITE"
-        resolve_conflicts_on_update = "OVERWRITE"
-      }
-      coredns = {
-        addon_version               = "v1.12.4-eksbuild.1"
-        resolve_conflicts_on_create = "OVERWRITE"
-        resolve_conflicts_on_update = "OVERWRITE"
-      }
-      eks-pod-identity-agent = {
-        addon_version               = "v1.3.10-eksbuild.2"
-        resolve_conflicts_on_create = "OVERWRITE"
-        resolve_conflicts_on_update = "OVERWRITE"
-      }
-      aws-ebs-csi-driver = {
-        addon_version = "v1.54.0-eksbuild.1"
-        pod_identity_association = [{
-          role_arn        = module.iam_roles["ebs-csi-driver-role"].role_arn
-          service_account = "ebs-csi-controller-sa"
-        }]
-      }
-    }
+#     # --------------------------------------------------------------------------
+#     # Cluster Add-ons
+#     # --------------------------------------------------------------------------
+#     cluster_addons = {
+#       vpc-cni = {
+#         addon_version               = "v1.20.4-eksbuild.1"
+#         resolve_conflicts_on_create = "OVERWRITE"
+#         resolve_conflicts_on_update = "OVERWRITE"
+#       }
+#       kube-proxy = {
+#         addon_version               = "v1.34.0-eksbuild.4"
+#         resolve_conflicts_on_create = "OVERWRITE"
+#         resolve_conflicts_on_update = "OVERWRITE"
+#       }
+#       coredns = {
+#         addon_version               = "v1.12.4-eksbuild.1"
+#         resolve_conflicts_on_create = "OVERWRITE"
+#         resolve_conflicts_on_update = "OVERWRITE"
+#       }
+#       eks-pod-identity-agent = {
+#         addon_version               = "v1.3.10-eksbuild.2"
+#         resolve_conflicts_on_create = "OVERWRITE"
+#         resolve_conflicts_on_update = "OVERWRITE"
+#       }
+#       aws-ebs-csi-driver = {
+#         addon_version = "v1.54.0-eksbuild.1"
+#         pod_identity_association = [{
+#           role_arn        = module.iam_roles["ebs-csi-driver-role"].role_arn
+#           service_account = "ebs-csi-controller-sa"
+#         }]
+#       }
+#     }
 
-    # --------------------------------------------------------------------------
-    # Global Tags
-    # --------------------------------------------------------------------------
-    tags = {
-      Purpose = "EKS-Cluster"
-    }
-  }
-}
+#     # --------------------------------------------------------------------------
+#     # Global Tags
+#     # --------------------------------------------------------------------------
+#     tags = {
+#       Purpose = "EKS-Cluster"
+#     }
+#   }
+# }
--- dev/outputs.tf
+++ dev/outputs.tf
@@ -194,183 +194,183 @@ output "iam_roles" {
   sensitive = true
 }
 
-# --------------------------------
-# EKS Cluster Outputs
-# --------------------------------
-output "eks_cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = module.dev_eks.cluster_name
-  sensitive   = true
-}
+# # --------------------------------
+# # EKS Cluster Outputs
+# # --------------------------------
+# output "eks_cluster_name" {
+#   description = "The name of the EKS cluster"
+#   value       = module.dev_eks.cluster_name
+#   sensitive   = true
+# }
 
-output "eks_cluster_id" {
-  description = "The ID of the EKS cluster"
-  value       = module.dev_eks.cluster_id
-  sensitive   = true
-}
+# output "eks_cluster_id" {
+#   description = "The ID of the EKS cluster"
+#   value       = module.dev_eks.cluster_id
+#   sensitive   = true
+# }
 
-output "eks_cluster_arn" {
-  description = "The Amazon Resource Name (ARN) of the EKS cluster"
-  value       = module.dev_eks.cluster_arn
-  sensitive   = true
-}
+# output "eks_cluster_arn" {
+#   description = "The Amazon Resource Name (ARN) of the EKS cluster"
+#   value       = module.dev_eks.cluster_arn
+#   sensitive   = true
+# }
 
-output "eks_cluster_endpoint" {
-  description = "The endpoint for the EKS cluster API server"
-  value       = module.dev_eks.cluster_endpoint
-  sensitive   = true
-}
+# output "eks_cluster_endpoint" {
+#   description = "The endpoint for the EKS cluster API server"
+#   value       = module.dev_eks.cluster_endpoint
+#   sensitive   = true
+# }
 
-output "eks_cluster_certificate_authority_data" {
-  description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = module.dev_eks.cluster_certificate_authority_data
-  sensitive   = true
-}
+# output "eks_cluster_certificate_authority_data" {
+#   description = "Base64 encoded certificate data required to communicate with the cluster"
+#   value       = module.dev_eks.cluster_certificate_authority_data
+#   sensitive   = true
+# }
 
-output "eks_cluster_version" {
-  description = "The Kubernetes server version of the cluster"
-  value       = module.dev_eks.cluster_version
-  sensitive   = true
-}
+# output "eks_cluster_version" {
+#   description = "The Kubernetes server version of the cluster"
+#   value       = module.dev_eks.cluster_version
+#   sensitive   = true
+# }
 
-output "eks_cluster_platform_version" {
-  description = "Platform version for the cluster"
-  value       = module.dev_eks.cluster_platform_version
-  sensitive   = true
-}
+# output "eks_cluster_platform_version" {
+#   description = "Platform version for the cluster"
+#   value       = module.dev_eks.cluster_platform_version
+#   sensitive   = true
+# }
 
-output "eks_cluster_status" {
-  description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
-  value       = module.dev_eks.cluster_status
-  sensitive   = true
-}
+# output "eks_cluster_status" {
+#   description = "Status of the EKS cluster. One of `CREATING`, `ACTIVE`, `DELETING`, `FAILED`"
+#   value       = module.dev_eks.cluster_status
+#   sensitive   = true
+# }
 
-output "eks_cluster_primary_security_group_id" {
-  description = "Cluster security group that was created by Amazon EKS for the cluster"
-  value       = module.dev_eks.cluster_primary_security_group_id
-  sensitive   = true
-}
+# output "eks_cluster_primary_security_group_id" {
+#   description = "Cluster security group that was created by Amazon EKS for the cluster"
+#   value       = module.dev_eks.cluster_primary_security_group_id
+#   sensitive   = true
+# }
 
-output "eks_cluster_security_group_id" {
-  description = "Security group ID attached to the EKS cluster control plane"
-  value       = module.dev_eks.cluster_security_group_id
-  sensitive   = true
-}
+# output "eks_cluster_security_group_id" {
+#   description = "Security group ID attached to the EKS cluster control plane"
+#   value       = module.dev_eks.cluster_security_group_id
+#   sensitive   = true
+# }
 
-output "eks_node_security_group_id" {
-  description = "Security group ID attached to the EKS nodes"
-  value       = module.dev_eks.node_security_group_id
-  sensitive   = true
-}
+# output "eks_node_security_group_id" {
+#   description = "Security group ID attached to the EKS nodes"
+#   value       = module.dev_eks.node_security_group_id
+#   sensitive   = true
+# }
 
-# --------------------------------
-# EKS OIDC Provider Outputs
-# --------------------------------
-output "eks_cluster_oidc_issuer_url" {
-  description = "The URL on the EKS cluster for the OpenID Connect identity provider"
-  value       = module.dev_eks.cluster_oidc_issuer_url
-  sensitive   = true
-}
+# # --------------------------------
+# # EKS OIDC Provider Outputs
+# # --------------------------------
+# output "eks_cluster_oidc_issuer_url" {
+#   description = "The URL on the EKS cluster for the OpenID Connect identity provider"
+#   value       = module.dev_eks.cluster_oidc_issuer_url
+#   sensitive   = true
+# }
 
-output "eks_oidc_provider" {
-  description = "The OpenID Connect identity provider (issuer URL without https://)"
-  value       = module.dev_eks.oidc_provider
-  sensitive   = true
-}
+# output "eks_oidc_provider" {
+#   description = "The OpenID Connect identity provider (issuer URL without https://)"
+#   value       = module.dev_eks.oidc_provider
+#   sensitive   = true
+# }
 
-output "eks_oidc_provider_arn" {
-  description = "The ARN of the OIDC Provider for IRSA"
-  value       = module.dev_eks.oidc_provider_arn
-  sensitive   = true
-}
+# output "eks_oidc_provider_arn" {
+#   description = "The ARN of the OIDC Provider for IRSA"
+#   value       = module.dev_eks.oidc_provider_arn
+#   sensitive   = true
+# }
 
-# --------------------------------
-# EKS IAM Role Outputs
-# --------------------------------
-output "eks_cluster_iam_role_name" {
-  description = "IAM role name of the EKS cluster"
-  value       = module.dev_eks.cluster_iam_role_name
-  sensitive   = true
-}
+# # --------------------------------
+# # EKS IAM Role Outputs
+# # --------------------------------
+# output "eks_cluster_iam_role_name" {
+#   description = "IAM role name of the EKS cluster"
+#   value       = module.dev_eks.cluster_iam_role_name
+#   sensitive   = true
+# }
 
-output "eks_cluster_iam_role_arn" {
-  description = "IAM role ARN of the EKS cluster"
-  value       = module.dev_eks.cluster_iam_role_arn
-  sensitive   = true
-}
+# output "eks_cluster_iam_role_arn" {
+#   description = "IAM role ARN of the EKS cluster"
+#   value       = module.dev_eks.cluster_iam_role_arn
+#   sensitive   = true
+# }
 
-output "eks_managed_node_group_iam_role_arns" {
-  description = "IAM role ARNs of the managed node groups"
-  value       = module.dev_eks.managed_node_group_iam_role_arns
-  sensitive   = true
-}
+# output "eks_managed_node_group_iam_role_arns" {
+#   description = "IAM role ARNs of the managed node groups"
+#   value       = module.dev_eks.managed_node_group_iam_role_arns
+#   sensitive   = true
+# }
 
-output "eks_self_managed_node_group_iam_role_arns" {
-  description = "IAM role ARNs of the self-managed node groups"
-  value       = module.dev_eks.self_managed_node_group_iam_role_arns
-  sensitive   = true
-}
+# output "eks_self_managed_node_group_iam_role_arns" {
+#   description = "IAM role ARNs of the self-managed node groups"
+#   value       = module.dev_eks.self_managed_node_group_iam_role_arns
+#   sensitive   = true
+# }
 
-# --------------------------------
-# EKS Node Groups Outputs
-# --------------------------------
-output "eks_managed_node_groups" {
-  description = "Map of outputs for all managed node groups created"
-  value       = module.dev_eks.managed_node_groups
-  sensitive   = true
-}
+# # --------------------------------
+# # EKS Node Groups Outputs
+# # --------------------------------
+# output "eks_managed_node_groups" {
+#   description = "Map of outputs for all managed node groups created"
+#   value       = module.dev_eks.managed_node_groups
+#   sensitive   = true
+# }
 
-output "eks_managed_node_groups_asg_names" {
-  description = "List of autoscaling group names created by managed node groups"
-  value       = module.dev_eks.managed_node_groups_autoscaling_group_names
-  sensitive   = true
-}
+# output "eks_managed_node_groups_asg_names" {
+#   description = "List of autoscaling group names created by managed node groups"
+#   value       = module.dev_eks.managed_node_groups_autoscaling_group_names
+#   sensitive   = true
+# }
 
-output "eks_self_managed_node_groups" {
-  description = "Map of outputs for all self-managed node groups created"
-  value       = module.dev_eks.self_managed_node_groups
-  sensitive   = true
-}
+# output "eks_self_managed_node_groups" {
+#   description = "Map of outputs for all self-managed node groups created"
+#   value       = module.dev_eks.self_managed_node_groups
+#   sensitive   = true
+# }
 
-output "eks_fargate_profiles" {
-  description = "Map of outputs for all Fargate profiles created"
-  value       = module.dev_eks.fargate_profiles
-  sensitive   = true
-}
+# output "eks_fargate_profiles" {
+#   description = "Map of outputs for all Fargate profiles created"
+#   value       = module.dev_eks.fargate_profiles
+#   sensitive   = true
+# }
 
-# --------------------------------
-# EKS KMS and CloudWatch Outputs
-# --------------------------------
-output "eks_kms_key_arn" {
-  description = "The ARN of the KMS key used for cluster encryption"
-  value       = module.dev_eks.kms_key_arn
-  sensitive   = true
-}
+# # --------------------------------
+# # EKS KMS and CloudWatch Outputs
+# # --------------------------------
+# output "eks_kms_key_arn" {
+#   description = "The ARN of the KMS key used for cluster encryption"
+#   value       = module.dev_eks.kms_key_arn
+#   sensitive   = true
+# }
 
-output "eks_cloudwatch_log_group_arn" {
-  description = "ARN of CloudWatch log group created for EKS cluster"
-  value       = module.dev_eks.cloudwatch_log_group_arn
-  sensitive   = true
-}
+# output "eks_cloudwatch_log_group_arn" {
+#   description = "ARN of CloudWatch log group created for EKS cluster"
+#   value       = module.dev_eks.cloudwatch_log_group_arn
+#   sensitive   = true
+# }
 
-# --------------------------------
-# EKS Configuration Outputs
-# --------------------------------
-output "eks_kubeconfig_command" {
-  description = "Command to update local kubeconfig"
-  value       = module.dev_eks.kubeconfig_command
-  sensitive   = true
-}
+# # --------------------------------
+# # EKS Configuration Outputs
+# # --------------------------------
+# output "eks_kubeconfig_command" {
+#   description = "Command to update local kubeconfig"
+#   value       = module.dev_eks.kubeconfig_command
+#   sensitive   = true
+# }
 
-output "eks_cluster_info" {
-  description = "Comprehensive EKS cluster information"
-  value = {
-    cluster_name         = module.dev_eks.cluster_name
-    cluster_endpoint     = module.dev_eks.cluster_endpoint
-    cluster_version      = module.dev_eks.cluster_version
-    oidc_provider        = module.dev_eks.oidc_provider
-    cluster_iam_role_arn = module.dev_eks.cluster_iam_role_arn
-    kubeconfig_command   = module.dev_eks.kubeconfig_command
-  }
-  sensitive = true
-}
+# output "eks_cluster_info" {
+#   description = "Comprehensive EKS cluster information"
+#   value = {
+#     cluster_name         = module.dev_eks.cluster_name
+#     cluster_endpoint     = module.dev_eks.cluster_endpoint
+#     cluster_version      = module.dev_eks.cluster_version
+#     oidc_provider        = module.dev_eks.oidc_provider
+#     cluster_iam_role_arn = module.dev_eks.cluster_iam_role_arn
+#     kubeconfig_command   = module.dev_eks.kubeconfig_command
+#   }
+#   sensitive = true
+# }
